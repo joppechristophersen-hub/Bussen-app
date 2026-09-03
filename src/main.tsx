fd775b92f4a3d063da432c42cbce170c0a15d6b5
Joppe Christophersen <joppe.christophersen@gmail.com>
2026-09-03 17:44:24 +0200

    Add playful Busbaas home and lobby styling

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,13 @@ import App from "./App.tsx";
 import CommerceShell from "./CommerceShell.tsx";
 import ExperienceShell from "./ExperienceShell.tsx";
 
-createRoot(
-  document.getElementById("root")!
-).render(
+/*
+ * Visuele Busbaas-polish bewust als laatste laden.
+ * Deze stylesheet is alleen gescoped op home + lobby.
+ */
+import "./homeLobbyPolish.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CommerceShell>
       <ExperienceShell>
