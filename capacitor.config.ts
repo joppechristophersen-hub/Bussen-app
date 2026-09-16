65536c58b28974f68ea210076fb95662d76d738a
Joppe Christophersen <joppe.christophersen@gmail.com>
2026-09-16 11:42:24 +0200

    Rename native package to BusBende

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.BusBaas.game',
+  appId: 'com.busbende.game',
   appName: 'BusBende',
   webDir: 'dist'
 };
